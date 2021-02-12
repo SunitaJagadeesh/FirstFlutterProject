3ffc3c8e306991bd490af9b4de13377df17e3c33
Sunita <sunitavangapandu@gmail.com>
2021-02-12 17:33:01 +0530

    My first commit

---
+++ android/app/src/main/kotlin/com/BrrSoftwares/scaffold/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.BrrSoftwares.scaffold
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: xyz()
+
+  ));
+}
+
+class xyz extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Brr Softwares'),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: Text('Hello Everyone',
+        style: TextStyle(
+          fontSize: 22,
+          color: Colors.red,
+          letterSpacing: 2,
+         ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        //onPressed: () {},
+        backgroundColor: Colors.blue,
+        child: Text('+',
+        style: TextStyle(
+          color: Colors.red,
+        ),),
+      ),
+    );
+  }
+}
